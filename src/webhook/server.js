646a233ca5635d52689d3409081c0107c1b38574
Korrumz2PL <korrumz2@gmail.com>
2022-02-16 22:57:07 +0100

    ok

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 module.exports = (client) => {
     app.post('/payload2', async (req, res) => {
         const { body } = req;
-        const { commits, head_commit, pusher, pull_request, issue, repository, after } = body;
+        const { commits, head_commit, pusher, pull_request, issue, repository, after, sender } = body;
         console.log(body);
 
         const channelID = "943261043415720026";
@@ -20,46 +20,36 @@ module.exports = (client) => {
             };
 
             let commit = new MessageEmbed()
-                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
+                .setAuthor({name: sender.login, iconURL: sender.avatar_url})
                 .setTitle(`(${repository.full_name}) New commits [${commitList.length}]`)
                 .setDescription(`Changes, full informations: https://github.com/Skyndalex/github-manager/commit/${after}`)
                 .addField(`Message(s)`, `\`${commitList.join(",\n")}\``, true)
                 .addField(`Modified file(s)`, `\`${head_commit.modified.join(",\n") || "None"}\``, true)
                 .setColor(`GREEN`)
             await client.channels.cache.get(channelID).send({embeds: [commit]})
+        } else if (!issue?.comment) {
+            let commentIssueNew = new MessageEmbed()
+                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
+                .setTitle(`(${repository.full_name}) New comment from issue: #${issue.title} [${issue.comments}]`)
+                .setDescription(`${issue.comment.body}`)
+                .setColor(`#cc33ff`)
+                .setURL(issue.comment.html_url)
+            await client.channels.cache.get(channelID).send({embeds: [commentIssueNew]})
         } else if (issue?.state === "open") {
             let issueEmbed = new MessageEmbed()
-                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
+                .setAuthor({ name: sender.login, iconURL: sender.avatar_url })
                 .setTitle(`(${repository.full_name}) Issue opened!`)
                 .setDescription(`${issue.body}`)
                 .addField(`Title`, `\`${issue.title}\``, true)
                 .addField(`Opened issues`, `\`${issue.number}\``, true)
-                .addField(`Author`, `\`${issue.user.login}\``, true)
                 .setColor(`GREEN`)
             await client.channels.cache.get(channelID).send({ embeds: [issueEmbed] })
         } else if (issue?.state === "closed") {
             let issueClosed = new MessageEmbed()
-                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
+                .setAuthor({ name: sender.login, iconURL: sender.avatar_url })
                 .setTitle(`(${body.repository.full_name}) Issue closed: #${issue.title}`)
                 .setColor(`RED`)
             await client.channels.cache.get(channelID).send({embeds: [issueClosed]})
-        } else if (pull_request?.state === "open") {
-            let pulLRequestNew = new MessageEmbed()
-                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
-                .setTitle(`(${repository.full_name}) New pull request #${pull_request.title}`)
-                .setURL(pull_request.html_url)
-                .setColor(`GREEN`)
-            await client.channels.cache.get(channelID).send({embeds: [pulLRequestNew]})
-        } else if (pull_request?.state === "closed") {
-            let pullRequestClosed = new MessageEmbed()
-                .setAuthor({
-                    name: body.sender.login,
-                    iconURL: body.sender.avatar_url
-                })
-                .setTitle(`(${repository.full_name}) Closed pull request #${pull_request.title}`)
-                .setURL(pull_request.html_url)
-                .setColor(`RED`)
-            await client.channels.cache.get(channelID).send({embeds: [pullRequestClosed]})
         }
     })
     app.get('/test', (req, res) => res.send('Hello World!'))
