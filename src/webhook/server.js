89fc8ced0ccba1456dd613009977227f78efa432
Korrumz2PL <korrumz2@gmail.com>
2022-02-17 23:57:05 +0100

    Update server.js

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json());
 module.exports = (client) => {
     app.post('/payload2', async (req, res) => {
         const { body } = req;
-        const { commits, head_commit, pusher, pull_request, issue, repository, after, sender } = body;
-        console.log(body);
-
+        const { commits, head_commit, pusher, pull_request, issue, comment, repository, after, sender } = body;
+        // console.log(body);
+        console.log(comment)
         const channelID = "943889804519673876";
 
         if (commits) {
@@ -27,31 +27,34 @@ module.exports = (client) => {
                 .addField(`Modified file(s)`, `\`${head_commit.modified.join(",\n") || "None"}\``, true)
                 .setColor(`GREEN`)
             await client.channels.cache.get(channelID).send({embeds: [embed]})
-        } else if (issue.commen ) {
-            let embed2 = new MessageEmbed()
-                .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
-                .setTitle(`(${repository.full_name}) New comment from issue: #${issue.title} [${issue.comments}]`)
-                .setDescription(`${issue.comment.body}`)
-                .setColor(`#cc33ff`)
-                .setURL(issue.comment.html_url)
-            await client.channels.cache.get(channelID).send({embeds: [embed2]})
-        } else if (issue.state === "open") {
+        } else if (comment) {
             let embed3 = new MessageEmbed()
+                .setAuthor({name: body.sender.login, iconURL: body.sender.avatar_url})
+                .setTitle(`(${repository.full_name}) New comment. Or edit. Or deleted.`)
+                .setFooter({
+                    text: "Sometimes there can be problems if the user performs a delete/edit/etc action. I also could not add the function name (Pull requests, commits etc)",
+                    iconURL: "https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png"
+                })
+                .setDescription(`\`${comment.body}\``)
+                .setColor(`#cc33ff`)
+                .setURL(comment.html_url)
+            await client.channels.cache.get(channelID).send({embeds: [embed3]})
+        } else if (issue?.state === "open") {
+            let embed1 = new MessageEmbed()
                 .setAuthor({ name: sender.login, iconURL: sender.avatar_url })
                 .setTitle(`(${repository.full_name}) Issue opened!`)
                 .setDescription(`${issue.body}`)
                 .addField(`Title`, `\`${issue.title}\``, true)
                 .addField(`Opened issues`, `\`${issue.number}\``, true)
                 .setColor(`GREEN`)
-            await client.channels.cache.get(channelID).send({ embeds: [embed3] })
-        } else if (issue.state === "closed") {
-            let embed4 = new MessageEmbed()
+            await client.channels.cache.get(channelID).send({ embeds: [embed1] })
+        } else if (issue?.state === "closed") {
+            let embed2 = new MessageEmbed()
                 .setAuthor({ name: sender.login, iconURL: sender.avatar_url })
                 .setTitle(`(${body.repository.full_name}) Issue closed: #${issue.title}`)
                 .setColor(`RED`)
-            await client.channels.cache.get(channelID).send({embeds: [embed4]})
+            await client.channels.cache.get(channelID).send({embeds: [embed2]})
         }
-
     })
     app.get('/test', (req, res) => res.send('Hello World!'))
 
