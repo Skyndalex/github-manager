4654c94e1dcc9683e37a62a7c235012435c02f45
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-18 15:56:25 +0200

    test

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -27,7 +27,7 @@ module.exports = (client) => {
 
             let embed = new MessageEmbed()
                 .setAuthor({name: body.sender.login, iconURL: body.sender.avatar_url})
-                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\nCommits:\n\n${commitListMessage.join(",\n")}\nModified: \`\`\`${head_commit.modified.join(",")}\`\`\``)
+                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}\n\nModified: \`\`\`${head_commit.modified.join(",")}\`\`\``)
                 .setColor(`GREEN`)
             await client.channels.cache.get(channelID).send({embeds: [embed]})
         } else if (comment) {
