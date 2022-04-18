77a6cae873823dd394163499876afce812ad4e77
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-18 15:58:05 +0200

    test, ale z większą ilością znaków

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -27,7 +27,7 @@ module.exports = (client) => {
 // test
             let embed = new MessageEmbed()
                 .setAuthor({name: body.sender.login, iconURL: body.sender.avatar_url})
-                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}\n\nModified: \`\`\`${head_commit.modified.join(",")}\`\`\``)
+                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}\n\nModified: \`\`\`${head_commit.modified.join(",\n")}\`\`\``)
                 .setColor(`GREEN`)
             await client.channels.cache.get(channelID).send({embeds: [embed]})
         } else if (comment) {
