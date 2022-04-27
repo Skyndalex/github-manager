0a860b3bbcbad2b170fe4eb1db1e90a3a5ed18a1
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-27 20:56:47 +0200

    refactor: change activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = client => {
     console.log(`Logged in as ${client.user.tag}`);
     client.guilds.cache.get("943884836169482260").commands.set(arrayOfSlashCommands)
 
-    client.user.setActivity('T E S T Y  N A  P R O D U K C J I', {
+    client.user.setActivity('github.com/Skyndalex', {
         status: "dnd",
         game: {
             name: "github.com/Skyndalex",
