f9b940e4aad3e7764ef52ed4ce111b3cf5a03916
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-14 23:16:24 +0200

    feat: fields, file action checking

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -21,8 +21,11 @@ module.exports = (client) => {
 
             let embed = new MessageEmbed()
                 .setAuthor({name: body.sender.login, iconURL: body.sender.avatar_url})
-                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}\n\nModified: \`\`\`ansi\n[1;33;40m${head_commit.modified.join(",\n") || "None"}\`\`\`\nAdded: \`\`\`ansi\n[1;32;40m${head_commit.added.join(",\n") || "None"}\`\`\`\nRemoved: \`\`\`ansi\n[1;31;40m${head_commit.removed.join(",\n") || "None"}\`\`\``)
+                .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}`)
                 .setColor(`GREEN`)
+            if (head_commit.modified.length > 0 ) embed.addField("Modified", `\`\`\`ansi\n[1;33;40m${head_commit.modified.join(",\n") || "None"}\`\`\``)
+            if (head_commit.added.length > 0 ) embed.addField("Added", `\`\`\`ansi\n[1;32;40m${head_commit.added.join(",\n") || "None"}\`\`\``)
+            if (head_commit.removed.length > 0 ) embed.addField("Removed", `\`\`\`ansi\n[1;31;40m${head_commit.removed.join(",\n") || "None"}\`\`\``)
             await client.channels.cache.get(channelID).send({embeds: [embed]})
         } else if (comment) {
             let embed8 = new MessageEmbed()
