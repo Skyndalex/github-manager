bb09c72d6db8654e9d157601d7e47807399f91e8
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-06-04 21:46:19 +0200

    weeeeeeeeeeee areeeeeeeee theeeeeeeee champioooooooooooons

---
+++ src/webhook/actions/getCommit.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+exports.run = async (client, body) => {
+    const { commits, head_commit, repository } = body;
+    console.log(body);
+
+    const channelID = "982001899903533076";
+
+    let commitListMessage = [];
+
+    for (let i in commits) {
+        commitListMessage.push(`[**\`${commits[i].message}\`**](${commits[i].url})`)
+    }
+
+    let embed = new MessageEmbed()
+        .setAuthor({ name: body.sender.login, iconURL: body.sender.avatar_url })
+        .setDescription(`Ref: ${body.ref}\nRepository: ${repository.full_name}\n\nCommits:\n${commitListMessage.join(",\n")}`)
+        .setColor(`GREEN`)
+    if (head_commit.modified.length > 0 ) embed.addField("Modified", `\`\`\`ansi\n[1;33;40m${head_commit.modified.join(",\n") || "None"}\`\`\``)
+    if (head_commit.added.length > 0 ) embed.addField("Added", `\`\`\`ansi\n[1;32;40m${head_commit.added.join(",\n") || "None"}\`\`\``)
+    if (head_commit.removed.length > 0 ) embed.addField("Removed", `\`\`\`ansi\n[1;31;40m${head_commit.removed.join(",\n") || "None"}\`\`\``)
+    await client.channels.cache.get(channelID).send({ embeds: [embed] })
+}
--- src/webhook/server.js
+++ src/webhook/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 7777;
-const { MessageEmbed } = require("discord.js");
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 module.exports = (client) => {
     app.post('/payload2', async (req, res) => {
         const { body } = req;
-        console.log(body)
 
-        await require("./newCommit").run(client, body)
-
+        await require("./actions/getCommit.js").run(client, body)
     })
 
     app.listen(port, () => console.log(`App listening at http://localhost:${ port }`))
